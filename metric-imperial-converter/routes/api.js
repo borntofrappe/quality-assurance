f68e2b5ad63a690bdb2e9c262246bc29d888911b
borntofrappe <borntofrappe@gmail.com>
2022-01-06 21:06:34 +0100

    api routes

--- metric-imperial-converter/routes/api.js
+++ metric-imperial-converter/routes/api.js
@@ -1,10 +1,31 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    const { input } = req.query;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string,
+    });
+  });
 };
