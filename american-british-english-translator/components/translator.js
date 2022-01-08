6530d52f9253000e6ae51016ed26d45da68bd535
borntofrappe <borntofrappe@gmail.com>
2022-01-08 22:29:39 +0100

    app route and logic

--- american-british-english-translator/components/translator.js
+++ american-british-english-translator/components/translator.js
@@ -1,10 +1,67 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  getAmericanTerms(text) {
+    const americanTerms = [];
 
+    Object.entries({
+      ...americanOnly,
+      ...americanToBritishSpelling,
+      ...americanToBritishTitles,
+    }).forEach(([american, british]) => {
+      if (new RegExp(`\\b${american}\\b`).test(text)) {
+        americanTerms.push([american, british]);
+      }
+    });
+
+    const times = text.match(/\d{1,2}:\d{1,2}/g) || [];
+    times.forEach((time) => americanTerms.push([time, time.replace(":", ".")]));
+
+    return americanTerms;
+  }
+
+  getBritishTerms(text) {
+    const britishTerms = [];
+
+    Object.entries({
+      ...americanOnly,
+      ...americanToBritishSpelling,
+      ...americanToBritishTitles,
+    }).forEach(([american, british]) => {
+      if (new RegExp(`\\b${british}\\b`).test(text)) {
+        britishTerms.push([british, american]);
+      }
+    });
+
+    Object.entries({
+      ...britishOnly,
+    }).forEach(([british, american]) => {
+      if (new RegExp(`\\b${british}\\b`).test(text)) {
+        britishTerms.push([british, american]);
+      }
+    });
+
+    const times = text.match(/\d{1,2}.\d{1,2}/g) || [];
+    times.forEach((time) => britishTerms.push([time, time.replace(".", ":")]));
+
+    return britishTerms;
+  }
+
+  translate(text, terms) {
+    let translation;
+
+    terms.forEach(([v1, v2]) => {
+      translation = text.replace(
+        new RegExp(`\\b${v1}\\b`, "g"),
+        `<span class="highlight">${v2}</span>`
+      );
+    });
+
+    return translation;
+  }
 }
 
 module.exports = Translator;
--- american-british-english-translator/routes/api.js
+++ american-british-english-translator/routes/api.js
@@ -1,13 +1,41 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    if (text === undefined || locale === undefined) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+    if (text.trim() === "") {
+      return res.json({ error: "No text to translate" });
+    }
+
+    const locales = ["american-to-british", "british-to-american"];
+    if (!locales.includes(locale)) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    const terms =
+      locale === "american-to-british"
+        ? translator.getAmericanTerms(text)
+        : translator.getBritishTerms(text);
+
+    if (terms.length === 0) {
+      return res.json({
+        text,
+        translation: "Everything looks good to me!",
+      });
+    }
+
+    const translation = translator.translate(text, terms);
+
+    res.json({
+      text,
+      translation,
     });
+  });
 };
