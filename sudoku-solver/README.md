c53908a18815caaaea13e1d55c843b4573aa9ee9
borntofrappe <borntofrappe@gmail.com>
2022-01-10 21:50:00 +0100

    add app logic through the SudokuSolver class

--- sudoku-solver/README.md
+++ sudoku-solver/README.md
@@ -1,1 +1,21 @@
-# [Sudoku Solver](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/sudoku-solver)
+# Sudoku Solver
+
+Create an application similar [to the example Metric-Imperial Converter](https://sudoku-solver.freecodecamp.rocks/).
+
+## Links
+
+- [Assignment](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-projects/sudoku-solver)
+
+- [REPL](https://replit.com/@borntofrappe/boilerplate-project-sudoku-solver)
+
+## App logic
+
+Complete the puzzle logic in `/controllers/sudoku-solver.js`. The class has several methods to validate, check and solve puzzle strings:
+
+- with `validate` check that the input string has 81 characters, includes only the accepted values, meaning numbers in the `[1, 9]` range and the dot character `.`. Finally check that there is no conflict in the rows, columns, regions
+
+- with `check*` validate a specific value in a specific coordinate. The three different functions illustrate if the puzzle string is valid relative to the row, column or again region
+
+- with `solve` solve a valid puzzle string
+
+Refer to `controllers/puzzle-strings.js` for a sample of puzzles the application should be able to handle.
--- sudoku-solver/controllers/sudoku-solver.js
+++ sudoku-solver/controllers/sudoku-solver.js
@@ -1,24 +1,106 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (
+      puzzleString.length !== 81 ||
+      new RegExp(/[^1-9\.]/).test(puzzleString)
+    ) {
+      return false;
+    }
+
+    for (let i = 0; i < puzzleString.length; i++) {
+      const value = puzzleString[i];
+      if (value !== ".") {
+        const column = i % 9;
+        const row = Math.floor(i / 9);
+
+        const pS = `${puzzleString.slice(0, i)}.${puzzleString.slice(i + 1)}`;
+
+        if (
+          !this.checkRowPlacement(pS, row, column, value) ||
+          !this.checkColPlacement(pS, row, column, value) ||
+          !this.checkRegionPlacement(pS, row, column, value)
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    const v = value.toString();
 
+    for (let c = 0; c < 9; c++) {
+      if (puzzleString[row * 9 + c] === v) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const v = value.toString();
 
+    for (let r = 0; r < 9; r++) {
+      if (puzzleString[column + r * 9] === v) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    const c = Math.floor(column / 3) * 3;
+    const r = Math.floor(row / 3) * 3;
+    const v = value.toString();
 
+    const s = c + r * 9;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (puzzleString[s + j + i * 9] === v) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  recursiveSolve(puzzleString) {
+    for (let i = 0; i < puzzleString.length; i++) {
+      if (puzzleString[i] === ".") {
+        const column = i % 9;
+        const row = Math.floor(i / 9);
+
+        for (let d = 1; d <= 9; d++) {
+          if (
+            this.checkRowPlacement(puzzleString, row, column, d) &&
+            this.checkColPlacement(puzzleString, row, column, d) &&
+            this.checkRegionPlacement(puzzleString, row, column, d)
+          ) {
+            if (!this.solution) {
+              this.recursiveSolve(
+                `${puzzleString.slice(0, i)}${d}${puzzleString.slice(i + 1)}`,
+                i + 1
+              );
+            }
+          }
+        }
+        return this.solution || false;
+      }
+    }
+    this.solution = puzzleString;
+    return puzzleString;
   }
 
   solve(puzzleString) {
-    
+    this.solution = null;
+    return this.recursiveSolve(puzzleString);
   }
 }
 
 module.exports = SudokuSolver;
-
